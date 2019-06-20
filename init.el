e959f1e917ed10cc0e3d7a586e145e945ecc1453
jester <soccerbeastwc@gmail.com>
2019-06-19 22:28:43 -0400

    Fireplace, fix magit commit

--- init.el
+++ init.el
@@ -16,7 +16,6 @@
 
 ;; multiple cursors
 ;; expand region
-;; helm
 ;; projectile
 ;; emmet
 
@@ -31,7 +30,7 @@
     ("e39ff005e524c331b08d613109bff0b55fc21c64914c4a243faa70f330015389" default)))
  '(package-selected-packages
    (quote
-    (evil-commentary evil-surround helm avy general magit linum-relative evil doom-modeline zerodark-theme which-key use-package))))
+    (fireplace evil-commentary evil-surround helm avy general magit linum-relative evil doom-modeline zerodark-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
