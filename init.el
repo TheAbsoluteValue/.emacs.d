754520301b1ca2ce8fff1a3b7b46b3bade95fc3a
will <wcarney@oakland.edu>
2019-07-03 16:49:10 -0400

    Added better Rust stuff also flycheck keeps yelling at me

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (require 'package)
 
+;;; Code:
 (add-to-list 'package-archives
          '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
@@ -26,13 +27,15 @@
  '(custom-safe-themes
    (quote
     ("e39ff005e524c331b08d613109bff0b55fc21c64914c4a243faa70f330015389" default)))
- '(inferior-lisp-program "/usr/bin/clisp")
  '(package-selected-packages
    (quote
-    (web-beautify flycheck rust-mode restclient helm-c-yasnippet yasnippet evil-multiedit neotree helm-projectile groovy-mode dashboard web-mode rjsx-mode company evil-mc fireplace evil-commentary evil-surround helm avy general magit linum-relative evil doom-modeline zerodark-theme which-key use-package))))
+    (cargo flycheck-rust racer flycheck rust-mode restclient helm-c-yasnippet yasnippet evil-multiedit neotree helm-projectile groovy-mode dashboard web-mode rjsx-mode company evil-mc fireplace evil-commentary evil-surround helm avy general magit linum-relative evil doom-modeline zerodark-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
